bc9538edf4ba32b386e0788d933a59587aeb7bf5
SONNY KEOBOUNPHANH <SKEOBOUNPHANH@GMAIL.COM>
2017-06-08 10:51:17 -0400

    complete character REST

--- game-of-thrones/app/controllers/characters_controller.rb
+++ game-of-thrones/app/controllers/characters_controller.rb
@@ -9,27 +9,37 @@ class CharactersController < ApplicationController
   end
 
   def new
+    @house = House.find(params[:house_id])
+    @character = @house.characters.new
 
   end
 
   def create
-
+    @house = House.find(params[:house_id])
+    @character = @house.characters.create(character_params)
+    redirect_to house_path(@house), notice: "You made a new character."
   end
 
   def edit
-
+    @house = House.find(params[:house_id])
+    @character = Character.find(params[:id])
   end
 
   def update
-
+    @character = Character.find(params[:id])
+    @character.update(character_params)
+    redirect_to house_character_path(@character), notice: "You updated the character."
   end
 
   def destroy
+    @character = Character.find(params[:id])
+    @character.destroy
+    redirect_to house_path(@character.house_id), notice: "You killed #{@character.name}."
 
   end
 
   private
-  def house_params
-    params.require(:house).permit(:name, :motto, :region, :crest_url)
+  def character_params
+    params.require(:character).permit(:name, :age, :image_url)
   end
 end
